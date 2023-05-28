e887391b0a90681bdcdac3cfac7f302ab6e85842
Mohammad <mohammadmghig@gmail.com>
2023-05-28 15:13:31 +0330

    group

--- envelope/routers.go
+++ envelope/routers.go
@@ -49,6 +49,7 @@ type PathGroup struct {
 	Path string
 	Method string
 	Handler http.Handler
+	
 }
  
 type PathRootGroup struct {
@@ -64,22 +65,40 @@ func (router *Router) Group(path string  ,fn func(r Router) Router ) {
 
 		for _, route := range fn(*router).routes{
 			if router.PathRootGroup.root.rightPath != nil{
-				router.PathRootGroup.root.rightPath = &PathGroup{Path : route.Pattern, rightPath: nil , leftPath : nil }
+				router.PathRootGroup.root.rightPath = &PathGroup{Path : route.Pattern, Handler: route.Handler ,rightPath: nil , leftPath : nil }
 			}else{
-				router.PathRootGroup.root.leftPath = &PathGroup{Path : route.Pattern, rightPath: nil , leftPath : nil  }
+				router.PathRootGroup.root.leftPath = &PathGroup{Path : route.Pattern,Handler: route.Handler , rightPath: nil , leftPath : nil  }
 			}
 
 		}	
 }
-
-
-
-func (r *Router) InsertRootPath(path string  ,routerCallBack func(r Router) Router ){
-	if path == pathRoot {
-		 
+ 
+func (r *PathGroup) SearchPathGroup(path string) PathGroup {
+	if r.Path ==path{
+		return *r
 	}
+	if r.rightPath.FindPath(path) == path{
+		return *r.rightPath
+	}
+	if r.leftPath.FindPath(path) == path{
+		return *r.leftPath
+	}
+	return PathGroup{}
 }
 
+ 
+func (r *PathGroup) FindPath(path string) string {
+	if r.Path ==path{
+		return path
+	}
+	if r.rightPath.FindPath(path) == path{
+		return r.rightPath.Path
+	}
+	if r.leftPath.FindPath(path) == path{
+		return r.leftPath.Path
+	}
+	return ""
+}
 
 
 func (r *Router) AddRoute(method, path string, handler http.Handler) {
@@ -87,8 +106,16 @@ func (r *Router) AddRoute(method, path string, handler http.Handler) {
 }
 
 func (r *Router) getHandler(method, path string) http.Handler {
-
- 
+	pathGroup := r.PathRootGroup.root.SearchPathGroup(path)
+	emptyPathGroup := PathGroup{}
+	if  pathGroup !=emptyPathGroup {
+		path = path + pathGroup.Path
+	 
+		if pathGroup.Method == method {
+			return pathGroup.Handler
+		}
+	}
+	
 
 	for _, route := range r.routes {
 		re := regexp.MustCompile(route.Pattern)
