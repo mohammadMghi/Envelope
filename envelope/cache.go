bd514d99f7fe3aefa4b84f55e3eaab29b0a1ce39
Mohammad <mohammadmghig@gmail.com>
2023-06-08 01:34:54 +0330

    cache

--- envelope/cache.go
+++ envelope/cache.go
@@ -7,34 +7,58 @@ const(
 )
 
 type Cache struct{
-	obj map[string]obj
+	object map[string]objects
+	defaultExpiration time.Duration
+}
+
+
+type objects struct{
+	item interface{}
 	timeExpression int64
 }
 
 
-type obj struct{
-	item interface{}
-
-}
-
-
-func (cache *Cache)set(key string ,item interface{} , timeExp time.Duration){
-	 
-
-	cache.timeExpression = int64(timeExp)
-
-	cache.obj[key] = obj{
-		item: item,
+func NewCacheEnv(defaultExpiration time.Duration)Cache{
+	obj := make(map[string]objects)
+	return Cache{
+		object : obj,
+		defaultExpiration :defaultExpiration,
 	}
 }
 
-func (cache *Cache)get(key string) interface{}{
-	if cache[key].timeExpression{}
-	
-	return cache[key]
+func (cache *Cache)set(key string ,item interface{} , timeExp time.Duration){
+	var exp int64
+	if timeExp == DefueltTimeExpetion{
+		timeExp = cache.defaultExpiration
+	}
+
+	if timeExp > 0 {
+		exp  = time.Now().Add(timeExp).UnixNano()
+	}
+
+	cache.object[key] = objects{
+		item: item,
+		timeExpression : exp,
+	}
 }
 
+func (cache *Cache)get(key string) (interface{} , bool){
 
-func (cache *cache)setDefueltTimeExpertion(){
-	DefueltTimeExpetion = 
+	//check the obj existed in cache
+	i , f := cache.object[key]
+
+	//object dose not existed
+	if f == false{
+		return nil , false
+	} 
+
+	if i.timeExpression < 0{
+		if !(time.Now().UnixNano() > i.timeExpression) {
+			return cache.object[key] , false
+		}
+	}
+
+	return nil ,false
+ 
 }
+ 
